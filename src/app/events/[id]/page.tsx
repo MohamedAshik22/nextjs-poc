e1a18b7a59d98551ce70fab187faed695e22717d
MohamedAshik22 <ashikmd256@gmail.com>
2024-08-10 13:04:55 +0530

    posts eliminated

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,5 +1,8 @@
 // app/events/[id]/page.tsx
+import React from 'react';
 import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 
 export interface Event {
   id: string;
@@ -24,6 +27,14 @@ const fetchEventById = async (id: string): Promise<Event> => {
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   const event = await fetchEventById(params.id);
 
+  if (!event) {
+    return {
+      title: 'Event Not Found',
+      description: 'The event you are looking for does not exist.',
+    };
+  }
+
+
   return {
     title: event.name,
     description: event.description,
@@ -41,3 +52,24 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
     },
   };
 }
+
+const EventDetail = async ({ params }: { params: { id: string } }) => {
+  const event = await fetchEventById(params.id);
+
+  if (!event) {
+    notFound(); // Redirect to 404 page if event is not found
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">{event.name}</h1>
+      <div className="flex justify-evenly mt-4">
+        <p>{event.date}</p>
+        <p>{event.location}</p>
+      </div>
+      <p className="mt-4">{event.description}</p>
+    </div>
+  );
+};
+
+export default EventDetail;
